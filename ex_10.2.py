10bd64f9171d708044dec972803a7ddc2e9faa03
Yanek K <yanek.dev@gmail.com>
2021-07-05 01:29:59 -0400

    Regex

--- ex_10.2.py
+++ ex_10.2.py
@@ -1,41 +1,19 @@
-fhand = input("Enter File Name")
-if len(fhand) < 1 : fhand = 'mbox-short.txt'
-f = open(fhand)
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = 'mbox-short.txt'
+fhand = open(fname)
 
-lst = list()
 di = dict()
-for line in f: 
+lst = list()
+for line in fhand : 
   if line.startswith("From "):
     wrd = line.split()
     h = wrd[5]
-    
     h = h.split(":")
     hours = h[0]
     lst.append(hours)
 for num in lst:
   di[num] = di.get(num, 0) + 1
 for hour, count in sorted(di.items()):
+  y = di.items()
+  print(y)
   print(hour, count)
-    
-# handle = open('mbox-short.txt')
-# x_list = list()
-# x_l = dict()
-
-# count = list
-# for line in handle:
-#    if line.startswith("From "):
-#         words = line.split()
-#         colon_col = words[5]
-         
-#         x,y,z = colon_col.split(":")
-#         x_list.append(x)
-
-# for num in x_list:
-#     if num not in x_l:
-#         x_l[num] = 1
-#     else:
-#         x_l[num] += 1
-
-
-# for key, val in list(sorted(x_l.items())):
-#     print(key,val)
---
+++ ex_11.1.py
@@ -0,0 +1,14 @@
+import re
+
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "regex_sum.txt"
+fhand = open(fname)
+inp = fhand.read()
+
+lst = list()
+num = re.findall('[0-9]+', inp)
+for number in num:
+  final = float(number)
+  lst.append(final)
+# if len(num) != 1 : continue
+print(sum(lst))
